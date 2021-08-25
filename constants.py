8bcf981690abc869fd13f8a21cb99b3caed8e0a8
Groverkss <groverkss@gmail.com>
2021-08-25 23:20:18 +0530

    Added docker integration

--- constants.py
+++ constants.py
@@ -1,12 +1,16 @@
 AUTO_UPDATE_TIME = 20
 
-SERVER_INVITE = "https://discord.gg/xP2UPUn"
-BOT_INVITE = "https://discord.com/oauth2/authorize?client_id=669978762120790045&permissions=0&scope=bot"
-GITHUB_LINK = "https://github.com/pseudocoder10/Lockout-Bot"
+SERVER_INVITE = ""
+BOT_INVITE = ""
+GITHUB_LINK = "https://github.com/IIIT-Programming-Club/Lockout-Bot"
 
-ADMIN_PRIVILEGE_ROLES = ['Admin', 'Moderator', 'Lockout Manager']
-OWNERS = [515920333623263252]
+ADMIN_PRIVILEGE_ROLES = ["Admin", "Moderator"]
+OWNERS = [
+    302519361141473290,
+    604549399724687364,
+    600916292668620800,
+    770282227052707890,
+    631485357094993962,
+]  # Kunwar, Amul, Rahul, Keyur, Vidit
 
-BACKUP_DIR = "./data/backup/"
-
-
+BACKUP_DIR = "/data/backup"
